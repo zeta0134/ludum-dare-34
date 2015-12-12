cce3127c72e4ccc9d879c61f2f173add9b44a055
Cristian Romo <cristian.g.romo@gmail.com>
2015-12-12 01:22:55 -0600

    Move key input to its own module

--- main.lua
+++ main.lua
@@ -2,72 +2,20 @@
 -- Sprite sheets
 -- Input
 
-key_map = {lctrl='left', rctrl='right'}
-keys_down = {left=false, right=false}
-function key_down()
-   if keys_down.left and keys_down.right then return 'both'
-   elseif keys_down.left then return 'left'
-   elseif keys_down.right then return 'right'
-   else return nil end
-end
+require "key"
 
-last_keys_down = nil
-function key_hit()
-   if key_down() ~= last_keys_down then
-      return key_down()
-   end
-   return nil
-end
-
-function love.keypressed(key, is_repeat)
-   for input, output in pairs(key_map) do
-      if key == input then
-	 keys_down[output] = true
-      end
-   end
-end
-
-function love.keyreleased(key, is_repeat)
-   for input, output in pairs(key_map) do
-      if key == input then
-	 keys_down[output] = false
-      end
-   end
-end
-
-state = 'straight'
-function update_drive_state()
-   if state == 'straight' then
-      if key_down() == 'left' then state = 'left'
-      elseif key_down() == 'right' then state = 'right'
-      end
-   elseif state == 'left' then
-      if not key_down() then state = 'straight'
-      elseif key_hit() == 'both' then state = 'slide-left'
-      end
-   elseif state == 'right' then
-      if not key_down() then state = 'straight'
-      elseif key_hit() == 'both' then state = 'slide-right'
-      end
-   elseif state == 'slide-left' then
-      if not key_down() then state = 'straight'
-      elseif key_hit() == 'both' then state = 'left'
-      end
-   elseif state == 'slide-right' then
-      if not key_down() then state = 'straight'
-      elseif key_hit() == 'both' then state = 'right'
-      end
-   end
+function love.load()
+   key.register_handlers(love)
 end
 
 function love.draw()
-   love.graphics.print("state: " .. state, 350, 270)
+   love.graphics.print("state: " .. key.state, 350, 270)
 end
 
 frame = 0
 function love.update(dt)
    require("lurker").update()
    frame = frame + 1
-   update_drive_state()
-   last_keys_down = key_down()
+   key.update_driver_state()
+   key.update()
 end
