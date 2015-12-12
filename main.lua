b80a55dcb5bf94b3a4bd2b18d49848c33e6819a9
Cristian Romo <cristian.g.romo@gmail.com>
2015-12-11 23:13:06 -0600

    Add basic input routines

--- main.lua
+++ main.lua
@@ -1,3 +1,37 @@
+-- Sound effects/music
+-- Sprite sheets
+-- Input
+
+key_map = {lctrl='left', rctrl='right'}
+keys_down = {left=false, right=false}
+function love.keypressed(key, is_repeat)
+   for input, output in pairs(key_map) do
+      if key == input then
+	 keys_down[output] = true
+      end
+   end
+end
+
+function love.keyreleased(key, is_repeat)
+   for input, output in pairs(key_map) do
+      if key == input then
+	 keys_down[output] = false
+      end
+   end
+end
+
 function love.draw()
-   love.graphics.print("Hello world!", 400, 300)
+   if keys_down.left then
+      love.graphics.print("left", 300, 300)
+   end
+   if keys_down.right then
+      love.graphics.print("right", 400, 300)
+   end
+   if keys_down.left and keys_down.right then
+      love.graphics.print("both", 350, 300)
+   end
+end
+
+function love.update()
+   require("lurker").update()
 end
