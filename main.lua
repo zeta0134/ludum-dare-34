7ae957e6ba4b567bf170a6c173d7d8761545f224
Cristian Romo <cristian.g.romo@gmail.com>
2015-12-12 15:38:41 -0600

    Add title logo

--- main.lua
+++ main.lua
@@ -22,8 +22,7 @@ function load(f)
    stage:load("art/bad-track.png")
    player:load()
 
-   --debug
-   s = sprites.new("sprite-test2")
+   title_logo = sprites.new "title-logo"
 end
 
 function love.load()
@@ -32,7 +31,7 @@ end
 
 lurker.postswap = load
 
-function love.draw()
+function game_draw()
    love.graphics.setCanvas()
    love.graphics.push()
    camera:apply_transform()
@@ -41,15 +40,22 @@ function love.draw()
    love.graphics.pop()
 
    love.graphics.print("state: " .. key.state, 350, 270)
-   love.graphics.print("frame: " .. frame, 350, 290)
-   s:draw(0, 0)
+end
+
+function title_draw()
+   title_logo:draw(love.window.getWidth() / 2, love.window.getHeight() / 4, nil, nil, nil, title_logo.sheet.image:getWidth() / 2, title_logo.sheet.image:getHeight() / 2)
+end
+
+function love.draw()
+   game_draw()
+   title_draw()
+   love.graphics.print("frame: " .. frame, 20, 570)
 end
 
 frame = 0
 function love.update(dt)
    require("lurker").update()
 
-   s:set_frame(frame / 15 % 2, frame / 30 % 4)
    -- sounds.play "doopadoo"
    if frame % 180 == 0 then
       -- sounds.play "woosh"
