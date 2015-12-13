6679dbb07907801c69454bf221812df97515b7d8
Cristian Romo <cristian.g.romo@gmail.com>
2015-12-13 15:56:15 -0600

    Add desert track

--- levels.lua
+++ levels.lua
@@ -3,21 +3,21 @@ local levels = {}
 
 levels["plains"] = {
    image_name="slightly-better-track",
-   starting_position=Vector.new(351,888),
+   starting_position=Vector.new(351, 888),
    starting_rotation=-0.5,
    checkpoints=18
 }
 
 levels["desert"] = {
-   image_name="slightly-better-track",
-   starting_position=Vector.new(351,888),
-   starting_rotation=0.5,
-   checkpoints=18
+   image_name="desert",
+   starting_position=Vector.new(691, 211),
+   starting_rotation=0.0,
+   checkpoints=14
 }
 
 levels["volcano"] = {
    image_name="slightly-better-track",
-   starting_position=Vector.new(351,888),
+   starting_position=Vector.new(351, 888),
    starting_rotation=-0.5,
    checkpoints=18
 }
